83d2395e50883dbac2fb8c1e7f09a6f6a51c8d19
Charles Thao <thaocharles23@gmail.com>
2024-05-23 21:11:07 -0500

    added form.js javascript, saving posts to local storage

---
+++ assets/js/form.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const blogForm = document.getElementById('blogForm');
+    const errorElement = document.getElementById('error');
+
+    if (blogForm) {
+        blogForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const username = document.getElementById('username').value;
+            const title = document.getElementById('title').value;
+            const content = document.getElementById('content').value;
+
+            if (username && title && content) {
+                errorElement.classList.add('hidden');
+                const post = { username, title, content };
+                savePostToLocalStorage(post);
+                window.location.href = 'blog.html';
+            } else {
+                errorElement.classList.remove('hidden');
+            }
+        });
+    }
+});
