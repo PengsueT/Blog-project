af1820ac018c58a5d00be0e903936a1ff6e48bad
Charles Thao <thaocharles23@gmail.com>
2024-05-23 20:17:13 -0500

    added javaScript in logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,14 @@
+function savePostToLocalStorage(post) {
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(posts));
+}
+
+function loadPostsForm() {
+    return JSON.parse(localStorage.getItem('posts')) || [];
+}
+
+function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+}
